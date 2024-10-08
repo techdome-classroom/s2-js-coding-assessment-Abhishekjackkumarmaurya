abc5a5f3d15f1864644a243161979fed30ac5910
Abhishekjackkumarmaurya <kumar841406shaharkola@gmail.com>
2024-10-08 14:04:08 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -8,7 +8,12 @@ var isValid = function(s) {
     map.set('{','}');
     map.set('(',')');
     map.set('[',']');
-
+     let t = 0;
+     for ( let i =0; i<s.length;i++){
+        for(let j=0; j<myArr.length;j++){
+            if(map.get)
+        }
+     }
 };
 
 module.exports = { isValid };
