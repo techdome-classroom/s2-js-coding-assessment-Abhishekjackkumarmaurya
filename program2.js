280f964852158a1721923cc7b8d1304cd17dee2b
Abhishekjackkumarmaurya <kumar841406shaharkola@gmail.com>
2024-10-08 14:38:43 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -16,7 +16,9 @@ var romanToInt = function(s) {
     for (let i=0; i<s.length-1;i++){
         if(rom[s[i]]< rom[s[i+1]]){
             count -= rom[s[i+1]];
-            
+
+        }else{
+            count
         }
     }
 };
