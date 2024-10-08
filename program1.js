24c79f3372e23c5af392fc9a6ac34133ece7dbf2
Abhishekjackkumarmaurya <kumar841406shaharkola@gmail.com>
2024-10-08 14:27:54 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,24 +3,7 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let stack=[];
-     let map = {
-        ')': '(',
-        '}': '{',
-        ']': '['
-    };
-
-    for (let i = 0; i < s.length; i++) {
-        let char = s[i];
-
-        if (char === '(' || char === '{' || char === '[') {
-            stack.push(char);
-        } else if (stack.length > 0 && stack[stack.length - 1] === map[char]) {
-            stack.pop();
-        } else {
-            return false;
-        }
-    }
+    
 
     return stack.length === 0;
 };
